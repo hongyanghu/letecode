24ea80879417f379d9cb0aa214bf896fa133832e
hhhhhhyyyyyyyy <997739480@qq.com>
2021-05-10 20:24:09 +0800

    无重复字符的最长字符串

---
+++ src/无重复字符的最长字符串/Solution.java
@@ -0,0 +1,58 @@
+package 无重复字符的最长字符串;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public int lengthOfLongestSubString(String s) {
+        int maxlen = 0;
+
+        ArrayList<Character> objects = new ArrayList<>();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            HashSet<Character> characters = new HashSet<>();
+            characters.add(chars[i]);
+            for (int j = i + 1; j < chars.length; j++) {
+                if (characters.contains(chars[j])) {
+                    break;
+                } else {
+                    characters.add(chars[j]);
+                }
+            }
+            if (characters.size() > maxlen) {
+                maxlen = characters.size();
+            }
+        }
+        return maxlen;
+    }
+    //双指针
+    public int lengthOfLongestSubstring2(String s) {
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet<Character>();
+        int n = s.length();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int abcabcbb = solution.lengthOfLongestSubString(" ");
+        System.out.println(abcabcbb);
+    }
+}
